2a73e2feb5b75f8819e3fc83c11745924cebcbcc
Manikandan9559 <manibcse@gmail.com>
2024-07-11 15:50:45 +0530

    Update EventCountColorCodeManager.java

--- colorcoder/EventCountColorCodeManager.java
+++ colorcoder/EventCountColorCodeManager.java
@@ -6,11 +6,11 @@ import colorcoder.MinorColorConstants.MINORCOLOR;
 public class EventCountColorCodeManager {
 
     public static void main(String[] args) {
-    	ColorCodePairPrinter.printNumberToPair(4, MAJORCOLOR.WHITE, MINORCOLOR.BROWN);
-    	ColorCodePairPrinter.printNumberToPair(5, MAJORCOLOR.WHITE, MINORCOLOR.SLATE);
+    	ColorCodePairPrinter.printCodeToPair(4, MAJORCOLOR.WHITE, MINORCOLOR.BROWN);
+    	ColorCodePairPrinter.printCodeToPair(5, MAJORCOLOR.WHITE, MINORCOLOR.SLATE);
     
-    	ColorCodePairPrinter.printPairToNumber(MAJORCOLOR.BLACK, MINORCOLOR.ORANGE, 12);
-    	ColorCodePairPrinter.printPairToNumber(MAJORCOLOR.VIOLET, MINORCOLOR.SLATE, 25);
+    	ColorCodePairPrinter.printPairToCode(MAJORCOLOR.BLACK, MINORCOLOR.ORANGE, 12);
+    	ColorCodePairPrinter.printPairToCode(MAJORCOLOR.VIOLET, MINORCOLOR.SLATE, 25);
     	
     	ColorCodeManualPrinter.printManual();
     }
